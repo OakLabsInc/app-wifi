b668fbc8bbf9585b695810d93d9ea59a60cf329a
Richard Joffray <rjoffray@Richards-MacBook-Pro.local>
2018-10-27 11:36:06 -0700

    added ripple touch

--- src/public/index.js
+++ src/public/index.js
@@ -24,6 +24,61 @@ window.app = window.angular
   })
 
 window.app.controller('appController', function ($log, $timeout, $scope, $http, $window, oak, _) {
+
+  // ripples
+  $scope.untapped = true
+  $scope.cursor = {
+    x: 0, y: 0
+  }
+  $scope.showCursor = false
+  $scope.cursorTimeout = 10000
+  var cursorPromises = []
+  var timer
+
+  // main window touches. this will log all tapped items, and also add the UI ripple of the tapped area
+  $scope.ripples = []
+
+  $scope.mouseMoved = function ({ pageX: x, pageY: y }) {
+    // dont show cursor if the settings has `false` or 0 as the cursorTimeout
+    if ($scope.cursorTimeout) {
+      resetCursorTimer()
+      $scope.cursor = { x, y }
+    }
+  }
+  var clearCursorPromises = function () {
+    cursorPromises.forEach(function (timeout) {
+      $timeout.cancel(timeout)
+    })
+    cursorPromises = []
+  }
+  var resetCursorTimer = function () {
+    clearCursorPromises()
+    $scope.showCursor = true
+    timer = $timeout(function () {
+      $scope.showCursor = false
+    }, $scope.cursorTimeout)
+    cursorPromises.push(timer)
+  }
+
+  $scope.$on('$destroy', function () {
+    clearCursorPromises()
+  })
+
+  $scope.tapped = function ({ pageX, pageY }) {
+    let id = $window.uuid.v4()
+    $scope.showCursor = false
+    $scope.ripples.push({
+      x: pageX, y: pageY, id
+    })
+    $timeout(function () {
+      _.remove($scope.ripples, { id })
+    }, 500)
+
+    if ($scope.untapped) {
+      $scope.untapped = false
+    }
+  }
+
   $scope.visible_access_points = []
   $scope.getAvailableWifi = function () {
     $http({
